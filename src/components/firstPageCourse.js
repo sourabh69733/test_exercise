a1edeb9c5cbcca6904a5b13017218d35fedb5e83
sourabh <sourabhsahu69733@gmail.com>
2021-06-06 00:20:48 +0530

    added finishing and more html tags

--- src/components/firstPageCourse.js
+++ src/components/firstPageCourse.js
@@ -1,24 +1,3 @@
-import React from 'react';
-import logo from "../logic-image.png";
-
-
-export default function FirstExcercisePage(props) {
-    return (
-      <div>
-
-        <div className="card" style={{ width: "18rem;" }}>
-          <img src={logo} className="card-img-top" alt="hi" style={{"width":"10px","height":"10px","background":"white","border":"none"}} />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:901fbdc7233cf9d90d5d407ee630b5caf425e8e12639a12dfbd09399bf660935
+size 2138
