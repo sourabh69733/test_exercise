c657e961b14989073e88c9f90f70f6606be63bf8
sourabh <sourabhsahu69733@gmail.com>
2021-06-05 08:45:17 +0530

    files added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { ProvideAuth } from "./components/use-auth.js";
 
 import Navbar from "./components/navbar.component";
 
@@ -8,8 +9,12 @@ import ExerciseList from "./components/exercise-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
+import CreateTestExercise from "./components/create-test.component";
+import {Login} from "./components/logingoogle";
+import SideBarComponent from "./components/sidebar.component";
 
-function App() {
+function App(props) {
+  
   return (
     <Router>
       <div className="container">
@@ -17,8 +22,17 @@ function App() {
         <br />
         <Route path="/" exact component={ExerciseList} />
         <Route path="/edit/:id" component={EditExercise} />
+        {/* <ProvideAuth> */}
+        {/* {(CreateExercise, CreateUser, CreateTestExercise)} */}
+        {/* </ProvideAuth> */}
         <Route path="/create" component={CreateExercise} />
         <Route path="/user" component={CreateUser} />
+        <Route path="/test-exercise" component={CreateTestExercise} />
+        <Route path="/login" component={Login} />
+        <Route
+          path="/lessons"
+          component={(SideBarComponent)}
+        />
       </div>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app";
+// import * as SideBarComponent from "./components/sidebar.component";
+import FirstExcercisePage from "./components/firstPageCourse";
+
 // import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
 
+ReactDOM.render(
+  <React.StrictMode>
+    <FirstExcercisePage />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 // If you want to change our app work offline then we can change
 // it unregrister to regrister
 // here is link  to learn more about service worker
