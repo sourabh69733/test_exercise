264bc8a2f868349419e5c8828d690d959636f42e
sourabh <sourabhsahu69733@gmail.com>
2021-06-22 11:34:23 +0530

    changes added

--- build/static/js/2.00ae8394.chunk.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:21f543e581e8fbedba24e9d8e5d2f58ec377a5c333301b4c5e84bbfd495864e0
-size 381061
--- build/static/js/main.7cf645b3.chunk.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f2faabaf7f257e379c1dcac8b676522e3f02247acf9ea358dda284f634755e21
-size 15952
