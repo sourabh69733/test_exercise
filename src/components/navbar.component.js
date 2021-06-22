cd37f778c87f1be2fc7d34cffa7b5a690185f169
sourabh <sourabhsahu69733@gmail.com>
2021-06-22 09:00:06 +0530

    Added static files images

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b928b817cabed4f403560b7149b15c4a88597a61a1913cabc01b3b6b3819576f
-size 1986
+oid sha256:3f15e5522fbdba53d2a9432087bf8e470c2e6292410e58da3f4ab2ad5d82d1e3
+size 1832
--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app";
-// import * as SideBarComponent from "./components/sidebar.component";
-import FirstExcercisePage from "./components/firstPageCourse";
-
-// import * as serviceWorker from "./serviceWorker";
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <FirstExcercisePage />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-// If you want to change our app work offline then we can change
-// it unregrister to regrister
-// here is link  to learn more about service worker
-
-// https: developers.google.com / web / fundamentals / primers / service - workers;
-
-// serviceWorker.unregister();
+version https://git-lfs.github.com/spec/v1
+oid sha256:b182cff693cba9aec5cf369927b911a50b1b59296c5de3e2f1b32c97e3571dbc
+size 825
